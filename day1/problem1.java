6d1971173a7084a39a32b7f5fe267b10b266380f
Shivkumargowdru <kshivakumar7227@gmail.com>
2021-08-09 23:03:28 +0530

    my first day activity

---
+++ day1/problem1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner; 
+public class problem1 { 
+
+static double calculateSal(double salary,double shifts){
+double savings=0;
+savings = (salary*0.5)+(salary*0.02*shifts); 
+return savings;
+
+}
+
+public static void main(String[] args) { 
+double salary=0,shifts=0; double savings=0;
+System.out.println("Enter salary and Shift "); 
+Scanner in=new Scanner(System.in); 
+salary = in.nextInt(); 
+shifts = in.nextInt(); 
+if(salary > 8000) 
+System.out.println("Salary too large "); 
+else if(shifts<0) 
+System.out.println("Shifts too small\n"); 
+else if(salary<0) 
+System.out.println("Salary too small"); 
+else { 
+savings=calculateSal(salary,shifts);
+System.out.println(savings);
+} 
+
+
+
+ 
+}
+
+}
+
+
