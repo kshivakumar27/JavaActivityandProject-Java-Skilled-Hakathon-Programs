f50c5723319aa808419f052bff1439f2feb24dd4
Shivakumar k <56855151+Shivkumargowdru@users.noreply.github.com>
2021-08-10 07:29:33 +0530

    initial commit

--- ActivityDay1/problem1.java
+++ ActivityDay1/problem1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner; 
 public class problem1 { 
 
-static double calculateSal(double salary,double shifts){
+static double calculateSal(double salary,int shifts){
 double savings=0;
 savings = (salary*0.5)+(salary*0.02*shifts); 
 return savings;
@@ -9,7 +9,7 @@ return savings;
 }
 
 public static void main(String[] args) { 
-double salary=0,shifts=0; double savings=0;
+double salary=0;int shifts=0; double savings=0;
 System.out.println("Enter salary and Shift "); 
 Scanner in=new Scanner(System.in); 
 salary = in.nextInt(); 
