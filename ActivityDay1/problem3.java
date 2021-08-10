6f93bbb35986d6dc6ec1299d7ec811b5a1bd91b2
Shivakumar k <56855151+Shivkumargowdru@users.noreply.github.com>
2021-08-10 07:34:18 +0530

    initial commit

--- ActivityDay1/problem3.java
+++ ActivityDay1/problem3.java
@@ -4,11 +4,12 @@ public static void main(String[] args) {
 
 int n,rem,prod=1; 
 Scanner in=new Scanner(System.in); 
+System.out.println("Enter the numbers ");
 n = in.nextInt(); 
 if(n<0 || n>32767) { 
-System.out.println("Invalid Input"); 
-System.exit(0); 
-}
+	System.out.println("Invalid Input"); 
+	qSystem.exit(0); 
+	}
 else
 {         
 while(n!=0)
