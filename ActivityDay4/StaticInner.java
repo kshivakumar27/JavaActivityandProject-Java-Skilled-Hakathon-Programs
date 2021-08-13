5376b8c7244647f8fcfd04cfedd66fe0b448ddfd
Shivakumar k <56855151+Shivkumargowdru@users.noreply.github.com>
2021-08-13 08:33:55 +0530

    commit

--- ActivityDay4/StaticInner.java
+++ ActivityDay4/StaticInner.java
@@ -3,43 +3,24 @@ package Activity1;
 public class StaticInner {
 	
 	
-		static int sum = 20;
-		
+	
 		
 		static class StaticInner1 {
-			public int D;
-			public int rem;
+			
 			
 			
 			public StaticInner1()
 	        {
 	            
-				D = 4;
-	            
-	            rem = sum%D;
-	        }
-			private int getD()
-	        {
-	            return D;
-	        }
-			private int getRem()
-	        {
-	            return rem;
-	        }
-			private int getQuotient()
-	        {
-	            System.out.println("inside static inner class");
-	            return sum / D;
-	        }
+			
+			}
 		
 	    
 
 	public static void main(String[] args) {
 	
 		StaticInner1 inner = new StaticInner1();
-		System.out.println("Divisor = "+ inner.getD());
-	    System.out.println("Remainder = " + inner.getRem());
-	    System.out.println("Quotient = " + inner.getQuotient());
+		
 
 		}
 	}
