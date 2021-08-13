b404eada9564ff96ab0b00ef95bb85cfcc105550
Shivakumar k <56855151+Shivkumargowdru@users.noreply.github.com>
2021-08-13 08:39:10 +0530

    commit

--- ActivityDay4/Java3.java
+++ ActivityDay4/Java3.java
@@ -1,47 +1,47 @@
 package Activity1;
 
 import java.util.Scanner;
+import java.util.*;
 
-import javax.naming.TeamNameNotFoundException;
+
+
+
+class TeamNameNotFoundException extends Exception{
+	public TeamNameNotFoundException(String str) {
+		super(str);
+	}
+}
+
 
 public class Java3 {
 	
-	public static void main(String[] args) {
-	String IPLteams="ChennaiSuperKings,SunRisersHyderabad,DelhiCapitals,KingsXIPunjab,KolkataKnightRiders,MumbaiIndians,RajasthanRoyal,RoyalChallengersBangalore";
-		String IPLteam1	="Chennai Super Kings";
-
-		String IPLteam2="Sun Risers Hyderabad";
-
-		String IPLteam3="Delhi Capitals";
-
-		String IPLteam4="Kings XI Punjab";
-
-		String IPLteam5="Kolkata Knight Riders";
-
-		String IPLteam6="Mumbai Indians";
-
-		String IPLteam7="Rajasthan Royals";
-		String IPLteam8="Royal Challengers Bangalore";
+	public static void main(String[] args) throws TeamNameNotFoundException{
+		String runner,winners;
 		
-		
-		System.out.println(" the expected winner team of IPL Season 13");
 		Scanner ui=new Scanner(System.in);
+	
+	String IPLteams[]={"ChennaiSuperKings","SunRisersHyderabad","DelhiCapitals","KingsXIPunjab","KolkataKnightRiders","MumbaiIndians","RajasthanRoyal","RoyalChallengersBangalore"};
+	try {
+		System.out.println(" the expected winner team of IPL Season 13");
+		
 		String input=ui.nextLine();
-		int s1=IPLteams.length();
-		int i=IPLteams.indexOf(input);
 		
-		if(i==-1) {
-			
-			System.out.println(" the expected winner team of IPL Season 13"+input);
-		}
-		else {
-					throw new TeamNameNotFoundException();
+		if(!Arrays.asList(IPLteams).contains(winners)) {
+			throw new TeamNameNotFoundException("Entered team is not a part of IPL Season 13");
 		}
 		
-			
-			System.out.println(" rcb");
-			
-			
+		System.out.println("Enter the expected runner Team of IPL Season 13");
+		runner= ui.nextLine();
+		if(!Arrays.asList(IPLteams).contains(runner)) {
+			throw new TeamNameNotFoundException("Entered team is not a part of IPL Season 13");
+		}
+		
+		System.out.println("Expected IPL Season 13 winner: " +winners);
+		System.out.println("Expected IPL Season 13 runner: " +runner);
+	} catch (TeamNameNotFoundException e) {
+		// TODO Auto-generated catch block
+		System.out.println(e);
+	}
 }
 		
 		
